cbcb061e82d46c3269e298da6b685ca100790cbe
CosmoKrista <krista.pitchford@gmail.com>
2025-12-12 06:49:49 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "fitness-board-v2";
+const ASSETS = [
+  "/fitness.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+
+  if (req.mode === "navigate") {
+    event.respondWith(
+      fetch(req).catch(() => caches.match("/fitness.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(res => res || fetch(req))
+  );
+});
