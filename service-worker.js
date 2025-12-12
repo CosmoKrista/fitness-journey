7fb2107c87fa5e0db7cb068c6803cdd16cdda6df
CosmoKrista <krista.pitchford@gmail.com>
2025-12-12 07:11:41 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,33 @@
-const CACHE_NAME = "fitness-board-v2";
+const CACHE_NAME = "fitness-journey-v2";
 const ASSETS = [
-  "/fitness.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  "./",
+  "./fitness.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => key !== CACHE_NAME && caches.delete(key))
-      )
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   const req = event.request;
 
   if (req.mode === "navigate") {
-    event.respondWith(
-      fetch(req).catch(() => caches.match("/fitness.html"))
-    );
+    event.respondWith(fetch(req).catch(() => caches.match("./fitness.html")));
     return;
   }
 
-  event.respondWith(
-    caches.match(req).then(res => res || fetch(req))
-  );
+  event.respondWith(caches.match(req).then((res) => res || fetch(req)));
 });
